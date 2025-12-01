27a866d385f6418acff16e680510b9af74617a87
dhurka3782 <dhurkamanchukan99@gmail.com>
2025-12-01 10:50:48 +0530

    logo updated

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Eye, EyeOff, ArrowRight, Loader2, Github, Twitter } from "lucide-react";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoApple } from "react-icons/io5";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import type { InteractionState } from "../App";
@@ -142,12 +144,7 @@ export function AuthForm({ isLogin, onToggleMode, onInteractionChange }: AuthFor
             className="flex px-3 h-11 items-center justify-center rounded-xl border border-border bg-card/40 hover:bg-card/60 text-sm font-medium text-white transition-all hover:border-white/20 active:scale-95"
           >
             {/* Custom Google SVG for multi-color support */}
-            <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
-              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
-              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
-              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
-            </svg>
+            <FcGoogle className="mr-2 h-4 w-4" />
             Google
           </button>
 
@@ -158,9 +155,7 @@ export function AuthForm({ isLogin, onToggleMode, onInteractionChange }: AuthFor
             className="flex h-11 px-3 items-center justify-center rounded-xl border border-border bg-card/40 hover:bg-card/60 text-sm font-medium text-white transition-all hover:border-white/20 active:scale-95"
           >
             {/* Custom Filled Apple SVG */}
-            <svg className="mr-2 h-4 w-4 fill-white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M17.05 20.28c-.98.95-2.05.88-3.08.4-1.09-.5-2.08-.48-3.24 0-1.44.62-2.2.44-3.06-.4C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.74 1.18 0 2.45-1.02 3.67-1.06 1.6-.05 3.19.74 3.89 1.81-3.49 1.85-2.9 6.55.5 7.97-.65 1.58-1.57 3.08-3.14 3.51zM12.03 5.39c-.16-2.58 2.04-4.38 4.21-4.39.38 2.65-2.3 4.67-4.21 4.39z" />
-            </svg>
+            <IoLogoApple className="mr-2 h-4 w-4" />
             Apple
           </button>
           <button className="flex h-11 px-3 items-center justify-center rounded-xl border border-border bg-card/40 hover:bg-card/60 text-sm font-medium text-white transition-all">
